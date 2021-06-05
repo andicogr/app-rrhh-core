7ac5fdde866cc965683e23f5b9135ba521158d55
Andres Gonzales <andicogr_01@hotmail.com>
2021-06-05 14:16:57 -0500

    Initial Commit

---
+++ src/main/java/com/business/rrhh/ComBusinessRrhhApplication.java
@@ -0,0 +1,13 @@
+package com.business.rrhh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComBusinessRrhhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComBusinessRrhhApplication.class, args);
+	}
+
+}
